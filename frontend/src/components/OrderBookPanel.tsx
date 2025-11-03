6ea5c38050bbb74e3d5650f7e53d34c5b298ad49
cnb.bQSeHDf4DZA <2472409840@qq.com>
2025-11-03 15:50:46 +0800

    调整买卖盘口图表

--- frontend/src/components/OrderBookPanel.tsx
+++ frontend/src/components/OrderBookPanel.tsx
@@ -24,10 +24,15 @@ export const OrderBookPanel: FC<OrderBookPanelProps> = ({ asks, bids, bestPrice 
         <span className="muted">单位：手</span>
       </div>
 
-      <div className="orderbook-list">
+      <div className="orderbook-list" style={{ marginTop: 16 }}>
+        <div className="orderbook-row" style={{ color: "#ff6b6b", fontWeight: 600 }}>
+          <span>卖盘</span>
+          <span style={{ width: 80, textAlign: "right" }}>价格</span>
+          <span style={{ flex: 1, textAlign: "right" }}>成交量</span>
+        </div>
         {asks.map((ask) => (
           <div key={`ask-${ask.price}`} className="orderbook-row">
-            <span style={{ width: 48, color: "#ff6b6b" }}>卖盘</span>
+            <span style={{ width: 48, color: "#ff6b6b" }}>{}</span>
             <span className="orderbook-price">{ask.price}</span>
             <div
               className="orderbook-bar sell"
@@ -43,9 +48,14 @@ export const OrderBookPanel: FC<OrderBookPanelProps> = ({ asks, bids, bestPrice 
       </div>
 
       <div className="orderbook-list">
+        <div className="orderbook-row" style={{ color: "#4ecdc4", fontWeight: 600 }}>
+          <span>买盘</span>
+          <span style={{ width: 80, textAlign: "right" }}>价格</span>
+          <span style={{ flex: 1, textAlign: "right" }}>成交量</span>
+        </div>
         {bids.map((bid) => (
           <div key={`bid-${bid.price}`} className="orderbook-row">
-            <span style={{ width: 48, color: "#4ecdc4" }}>买盘</span>
+            <span style={{ width: 48, color: "#4ecdc4" }}>{}</span>
             <span className="orderbook-price">{bid.price}</span>
             <div
               className="orderbook-bar buy"
